923d9b9192826364f28c61ba928f7ab82f77ce76
kabeer Akanni <77249754+kabeer-prog@users.noreply.github.com>
2022-07-06 03:24:22 +0100

    Create Student files

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Contains the pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal's triangle of n"""
+
+    if n <= 0:
+        return[]
+
+    triangle = [[1]]
+    while len(triangle) != n:
+        tri = triangle[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangle.append(tmp)
+    return(triangle)

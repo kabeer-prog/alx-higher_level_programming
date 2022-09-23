c344b0dc23fd5b91245646004776e54fa3f4fbce
kabeer Akanni <77249754+kabeer-prog@users.noreply.github.com>
2022-09-23 06:02:03 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script that takes in a URL and sends a POST request to the URL
-curl -sL -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
